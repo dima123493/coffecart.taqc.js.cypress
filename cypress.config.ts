a781668648da0cdfdbb674108e0c76a18d2dbe20
Dmytro Zubenko <fokist@outlook.com>
2025-05-30 02:38:32 +0300

    config modified

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,8 +1,10 @@
-import { defineConfig } from "cypress";
+import { defineConfig } from 'cypress';
 import allureWriter from '@shelex/cypress-allure-plugin/writer';
 
 export default defineConfig({
   e2e: {
+    specPattern: 'tests/**/*.cy.{js,ts,jsx,tsx}',
+    supportFile: 'cypress/support/e2e.ts',
     setupNodeEvents(on, config) {
       allureWriter(on, config);
       return config;
