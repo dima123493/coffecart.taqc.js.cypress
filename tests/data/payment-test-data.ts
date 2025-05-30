fe9e75ca9979830bf0cc6781f0fd3bb327cfc4dd
Dmytro Zubenko <fokist@outlook.com>
2025-05-30 23:07:51 +0300

    test data separated, test updated

---
+++ tests/data/payment-test-data.ts
@@ -0,0 +1,7 @@
+export const PaymentTestData = {
+    validName: 'Andrii',
+    validEmail: 'andrii11@gmail.com',
+    testingDrink: 'Flat White',
+    successMessage: 'Thanks for your purchase. Please check your email for payment.',
+    emptyCartText: 'Total: $0.00',
+};
--- tests/payment-form-test.cy.ts
+++ tests/payment-form-test.cy.ts
@@ -1,32 +1,27 @@
 import {MenuPage} from '@pages/menu-page';
 import {PaymentModal} from '@modal/payment-detail-modal';
 import {SuccessfulModal} from '@modal/successful-modal';
+import { PaymentTestData as Data } from '@tests/data/payment-test-data';
 
 const menuPage = new MenuPage();
 const paymentModal = new PaymentModal();
 const successModal = new SuccessfulModal();
 
 describe('Verify Payment Form Accepts Valid Inputs', () => {
-    const VALID_NAME = 'Andrii';
-    const VALID_EMAIL = 'andrii11@gmail.com';
-    const TESTING_DRINK_NAME = 'Flat White';
-    const SUCCESS_MESSAGE = 'Thanks for your purchase. Please check your email for payment.';
-    const EMPTY_CART_VALUES = 'Total: $0.00';
-
     beforeEach(() => {
         menuPage.visit();
     });
 
     it('should submit payment form with valid inputs', () => {
-        menuPage.clickOnDrink(TESTING_DRINK_NAME);
+        menuPage.clickOnDrink(Data.testingDrink);
         menuPage.clickTotalButton();
         menuPage.verifyPaymentModalAppears();
 
-        paymentModal.enterName(VALID_NAME);
-        paymentModal.enterEmail(VALID_EMAIL);
+        paymentModal.enterName(Data.validName);
+        paymentModal.enterEmail(Data.validEmail);
         paymentModal.clickSubmit();
 
-        successModal.getSuccessMessage().should('eq', SUCCESS_MESSAGE);
-        menuPage.verifyTotalButtonText(EMPTY_CART_VALUES);
+        successModal.getSuccessMessage().should('eq', Data.successMessage);
+        menuPage.verifyTotalButtonText(Data.emptyCartText);
     });
 });
