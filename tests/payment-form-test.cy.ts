042325b4eb03eb777ee747cdc34a405da7684697
Dmytro Zubenko <fokist@outlook.com>
2025-05-30 23:25:47 +0300

    allure annotations added

--- tests/payment-form-test.cy.ts
+++ tests/payment-form-test.cy.ts
@@ -6,19 +6,25 @@ import { PaymentTestData as Data } from '@tests/data/payment-test-data';
 const menuPage = new MenuPage();
 const paymentModal = new PaymentModal();
 const successModal = new SuccessfulModal();
-
+cy.allure().feature('Verify Payment Form Accepts Valid Inputs Feature');
 describe('Verify Payment Form Accepts Valid Inputs', () => {
     beforeEach(() => {
         menuPage.visit();
     });
 
     it('should submit payment form with valid inputs', () => {
+        cy.allure().step(`Click drink named ${Data.testingDrink}`, false);
         menuPage.clickOnDrink(Data.testingDrink);
+        cy.allure().step(`Click total button`, false);
         menuPage.clickTotalButton();
+        cy.allure().step(`Verify payment modal appears`, false);
         menuPage.verifyPaymentModalAppears();
 
+        cy.allure().step(`Enter user name ${Data.validName}`, false);
         paymentModal.enterName(Data.validName);
+        cy.allure().step(`Enter user email ${Data.validEmail}`, false);
         paymentModal.enterEmail(Data.validEmail);
+        cy.allure().step(`Click submit`, false);
         paymentModal.clickSubmit();
 
         successModal.getSuccessMessage().should('eq', Data.successMessage);
