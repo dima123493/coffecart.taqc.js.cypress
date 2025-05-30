7561702c18fcb9bf05faa4fad74c0d19ccc6d40d
Наталія <138515375+HrushaNataliia@users.noreply.github.com>
2025-05-30 18:40:48 +0300

    Increase Items in Cart

--- cypress.config.ts
+++ cypress.config.ts
@@ -4,10 +4,6 @@ export default defineConfig({
   e2e: {
     specPattern: 'tests/**/*.cy.{js,ts,jsx,tsx}',
     supportFile: 'cypress/support/e2e.ts',
-    setupNodeEvents(on, config) {
-      allureWriter(on, config);
-      return config;
-    },
     env: {
       allure: true,
       allureReuseAfterSpec: true,
--- cypress/support/e2e.ts
+++ cypress/support/e2e.ts
@@ -1,2 +1,1 @@
-import '@shelex/cypress-allure-plugin';
 import 'cypress-xpath';
