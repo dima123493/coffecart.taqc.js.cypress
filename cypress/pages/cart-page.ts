1d2d7ab7230cff74e94bd5828295f80ea4d97e72
Roman <romankmet10@gmail.com>
2025-06-02 07:27:43 +0300

    [TC7] Verification of pop-up cart functioning (buttons +/-)

--- cypress/pages/cart-page.ts
+++ cypress/pages/cart-page.ts
@@ -3,8 +3,8 @@ export class CartPage {
     private totalButtonLocator = "//button[contains(text(), 'Total')]";
     private cartDataLocator = "//*[@id='app']/div[2]/div/ul/li[2]/div[2]/span";
     private cartAmountLocator = "//*[@id='app']/ul/li[2]/a";
-    private plusButtonLocator = "//*[@id='app']/div[2]/div/ul/li[2]/div[2]/div/button[1]";
-    private minusButtonLocator = "//*[@id='app']/div[2]/div/ul/li[2]/div[2]/div/button[2]";
+    private plusButtonLocator = "//*[@id=\"app\"]/div[2]/div[1]/ul/li/div[2]/button[1]";
+    private minusButtonLocator = "//*[@id=\"app\"]/div[2]/div[1]/ul/li/div[2]/button[2]";
     private noCoffeeGoAddSome = "//*[@id='app']/div[2]/p";
     private totalPriceOfItem = "//*[@id='app']/div[2]/div/ul/li[2]/div[3]";
 
@@ -38,5 +38,9 @@ export class CartPage {
 
     cartStatus(){
         return cy.xpath(this.noCoffeeGoAddSome)
+    }
+
+    mouseOverTotalButton() {
+        cy.xpath(this.totalButtonLocator).trigger('mouseover');
     }
 }
